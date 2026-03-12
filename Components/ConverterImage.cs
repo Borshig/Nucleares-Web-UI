58b3d30663d3e4f379583a236d34b744fb3db0ef
Frontasts <steamfronts0110@gmail.com>
2026-03-12 03:32:46 +0300

    Add support image to base64

---
+++ Components/ConverterImage.cs
@@ -0,0 +1,24 @@
+﻿
+namespace NuclearesWebUI.Components
+{
+    public class Helper
+    {
+        public static string ConverterImageToBase64(string filepath)
+        {
+            byte[] imageByte = File.ReadAllBytes(filepath);
+            string base64 = Convert.ToBase64String(imageByte);
+
+            string mimeType = Path.GetExtension(filepath).ToLower() switch
+            {
+                ".png" => "image/png",
+                ".jpg" or ".jpeg" => "image/jpeg",
+                ".gif" => "image/gif",
+                ".webp" => "image/webp",
+                ".svg" => "image/svg+xml",
+                _ => "application/octet-stream"
+            };
+
+            return $"data:{mimeType};base64,{base64}";
+        }
+    }
+}
